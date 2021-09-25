1f5d355fea05d6f96ad9c723c300cd64256c51b0
bandgarmamta <bandgarmamta@gmail.com>
2021-09-25 21:36:40 +0530

    doc: updated file

--- index.js
+++ index.js
@@ -1,57 +1,59 @@
 var readlineSync = require("readline-sync")
 var userName = readlineSync.question("Your name please: ");
-console.log("Welcome "+userName+ "! Let's see how well do you know mamta");
+console.log("Welcome " + userName + "! Let's see how well do you know mamta");
 console.log("---------------------------------")
 
 
 var score = 0;
 
 var questions = [
-  {
-    question:"Where do I live? ",
-    answer:"Sakinaka"},
-
-  {
-    question:"Which is my favourite song?",
-    answer:"Lag jaa gale"
+    {
+        question: "Where do I live? ",
+        answer: "Sakinaka"
     },
-  {
-    question:"What is my favourite color? ",
-    answer:"black"},
-  {
-    question:"Who is my favourite player? ",
-    answer:"Dhoni"},
-  {
-    question:"What is my favourite food? ",
-    answer:"Pav bhaji"
-  },
-  {
-    question:"Do i like oggy and the cockroaches? ",
-    answer:"No"
-  },
-  ]
 
-for (var i = 0;i<questions.length;i++){
-   game(questions[i].question, questions[i].answer)
-   console.log("---------------------------------")
-  }
+    {
+        question: "Which is my favourite song?",
+        answer: "Lag jaa gale"
+    },
+    {
+        question: "What is my favourite color? ",
+        answer: "black"
+    },
+    {
+        question: "Who is my favourite player? ",
+        answer: "Dhoni"
+    },
+    {
+        question: "What is my favourite food? ",
+        answer: "Pav bhaji"
+    },
+    {
+        question: "Do i like oggy and the cockroaches? ",
+        answer: "No"
+    },
+]
 
-console.log("Your final score is: "+score)
-
-  
-function game(question, answer){
-  var userAnswer = readlineSync.question(question);
-  if(userAnswer.toUpperCase() === answer.toUpperCase())
-  {
-    score = score + 1;
-    console.log("Your answer is absoultly right!\nYour current score is: "+score);
-
-  }
-  else{
-    console.log("You don't know me man!\nYour current score is: "+score)
-
-  }
+for (var i = 0; i < questions.length; i++) {
+    game(questions[i].question, questions[i].answer)
+    console.log("---------------------------------")
 }
-  
+
+console.log("Your final score is: " + score)
+
+
+function game(question, answer) {
+    var userAnswer = readlineSync.question(question);
+    if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+        score = score + 1;
+        console.log("Your answer is absoultly right!\nYour current score is: " + score);
+
+    }
+    else {
+        console.log("You don't know me man!\nYour current score is: " + score)
+
+    }
+}
+
 
 
