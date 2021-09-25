e58816e19801cc9bfd8d7511e3d88d7beeebf263
bandgarmamta <bandgarmamta@gmail.com>
2021-09-25 21:39:00 +0530

    fix: indentation

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ var userName = readlineSync.question("Your name please: ");
 console.log("Welcome " + userName + "! Let's see how well do you know mamta");
 console.log("---------------------------------")
 
-
 var score = 0;
+
+function game(question, answer) {
+    var userAnswer = readlineSync.question(question);
+    if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+        score = score + 1;
+        console.log("Your answer is absoultly right!\nYour current score is: " + score);
+
+    }
+    else {
+        console.log("You don't know me man!\nYour current score is: " + score)
+
+    }
+}
 
 var questions = [
     {
@@ -40,20 +52,5 @@ for (var i = 0; i < questions.length; i++) {
 }
 
 console.log("Your final score is: " + score)
-
-
-function game(question, answer) {
-    var userAnswer = readlineSync.question(question);
-    if (userAnswer.toUpperCase() === answer.toUpperCase()) {
-        score = score + 1;
-        console.log("Your answer is absoultly right!\nYour current score is: " + score);
-
-    }
-    else {
-        console.log("You don't know me man!\nYour current score is: " + score)
-
-    }
-}
-
 
 
